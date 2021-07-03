346521de49a97f2c324c53d8d76b2000ffda28dc
um4ng-tiw <utiwari18@gmail.com>
2021-07-03 19:45:56 +0530

    displayed show results and small fetch refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,44 @@
 import React, { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
+import { apiGet } from "../misc/config";
+
 const Home = () => {
   const [input, setInput] = useState("");
+
+  const [results, setResults] = useState(null);
+
+  const onSearch = () => {
+    apiGet(`/search/shows?q=${input}`).then((result) => {
+      setResults(result);
+      console.log(result);
+    });
+  };
 
   const onInputChange = (ev) => {
     setInput(ev.target.value);
   };
 
-  const onSearch = () => {
-    fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
-      .then((r) => r.json())
-      .then((result) => console.log(result));
-  };
   const onKeyDown = (ev) => {
     if (ev.keyCode === 13) {
       onSearch();
     }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map((item) => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
   return (
     <MainPageLayout>
@@ -28,6 +51,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
