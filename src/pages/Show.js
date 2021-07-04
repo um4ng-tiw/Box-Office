08ba21c34a536e2a604c0c00a57b62494f89abff
um4ng-tiw <utiwari18@gmail.com>
2021-07-04 20:31:00 +0530

    created two new more hooks: useLastQuery and useShow

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,65 +1,16 @@
-import React, { useEffect, useReducer } from "react";
 import { useParams } from "react-router";
-import { apiGet } from "../misc/config";
 import ShowMainData from "../components/show/ShowMainData";
 import Details from "../components/show/Details";
 import Seasons from "../components/show/Seasons";
 import Cast from "../components/show/Cast";
 import { ShowPageWrapper } from "./Show.styled";
 import { InfoBlock } from "./Show.styled";
-
-const reducer = (prevState, action) => {
-  switch (action.type) {
-    case "FETCH_SUCCESS": {
-      return { ...prevState, isLoading: false, error: null, show: action.show };
-    }
-
-    case "FETCH_FAILED": {
-      return { ...prevState, isLoading: false, error: action.error };
-    }
-    default:
-      return prevState;
-  }
-};
-
-const initialState = {
-  show: null,
-  isLoading: true,
-  error: null,
-};
+import { useShow } from "../misc/custom-hooks";
 
 const Show = () => {
   const { id } = useParams();
 
-  const [{ show, isLoading, error }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-
-  //   const [show, setShow] = useState(null);
-  //   const [isLoading, setIsLoading] = useState(true);
-  //   const [error, setError] = useState(null);
-  useEffect(() => {
-    let isMounted = true;
-    apiGet(`shows/${id}?embed[]=seasons&embed[]=cast`)
-      .then((results) => {
-        if (isMounted) {
-          dispatch({ type: "FETCH_SUCCESS", show: results });
-        }
-      })
-      .catch(
-        (err) => {
-          if (isMounted) {
-            dispatch({ type: "FETCH_FAILED", error: err.message });
-          }
-        },
-        [id]
-      );
-
-    return () => {
-      isMounted = false;
-    };
-  }, [id]);
+  const { show, isLoading, error } = useShow(id);
 
   if (isLoading) {
     return <div>Data is being loaded</div>;
